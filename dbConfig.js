2ea35fef00d2fa1943f5a513ce0ebe9b03e0c016
Richard Widjaja <94299579+nightravenx@users.noreply.github.com>
2023-01-18 06:41:30 -0800

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -4,10 +4,10 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://postgres:vdMZLdU25Y1ycVEIK5r4@containers-us-west-43.railway.app:6394/railway`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: 'postgresql://postgres:vdMZLdU25Y1ycVEIK5r4@containers-us-west-43.railway.app:6394/railway',
   ssl: isProduction
 });
 
